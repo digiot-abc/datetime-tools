fc632e4a5200a16961d17271701744f71dd388f8
digiot <digiot.abc@gmail.com>
2023-11-14 17:33:09 +0900

    初期修正

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/project-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
